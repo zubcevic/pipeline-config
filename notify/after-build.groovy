6f0353a504121e7b58e524719b007ee8c54eb72b
zubcevic <git@zubcevic.com>
2020-10-16 10:40:41 +0200

    notify added

--- notify/after-build.groovy
+++ notify/after-build.groovy
@@ -4,5 +4,5 @@ void call(context){
         execute something right before the library step called
         build is executed.
     */
-    println("build step finished: ${context.status}")
+    println("build step finished: ${currentBuild.result}")
 }
