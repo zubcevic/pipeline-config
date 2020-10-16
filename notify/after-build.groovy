b3ab93a83dbaee532bd09676ab39b14bce952224
zubcevic <git@zubcevic.com>
2020-10-16 13:02:28 +0200

    notify added

--- notify/after-build.groovy
+++ notify/after-build.groovy
@@ -4,6 +4,16 @@ import hudson.model.Cause;
 import hudson.model.Cause.UserIdCause;
 
 @AfterStep({ context.step.equals("build") })
+void callBefore(context){
+    def cause = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)
+    def id = cause.getUserId()
+    User u = User.get(id)
+    def umail = u.getProperty(Mailer.UserProperty.class)
+    
+    println("build step started by: ${umail.getAddress()}")
+}
+
+@AfterStep({ context.step.equals("build") })
 void call(context){
     /*
         execute something right before the library step called
