0142f3867bbd39137151782a7fd06f52f2e6d8e2
zubcevic <git@zubcevic.com>
2020-10-16 11:01:49 +0200

    notify added

--- notify/after-build.groovy
+++ notify/after-build.groovy
@@ -1,3 +1,8 @@
+import hudson.tasks.Mailer;
+import hudson.model.User;
+import hudson.model.Cause;
+import hudson.model.Cause.UserIdCause;
+
 @AfterStep({ context.step.equals("build") })
 void call(context){
     /*
@@ -5,4 +10,12 @@ void call(context){
         build is executed.
     */
     println("build step finished: ${currentBuild.currentResult}")
+
+    def cause = build.getCause(hudson.model.Cause$UserIdCause)
+    def id = cause.getUserId()
+    User u = User.get(id)
+    def umail = u.getProperty(Mailer.UserProperty.class)
+    print umail.getAddress()
+
+    mail bcc:'',body:'Test',cc:'',from:'',replyTo:'',subject:"Build result ${currentBuild.currentResult}",to:umal.getAddress()
 }
