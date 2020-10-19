70ffdf6a9b5787e62ce60cc77def71f92e0054e6
zubcevic <git@zubcevic.com>
2020-10-19 12:38:15 +0200

    added deploy_to

---
+++ docker/deploy_to.groovy
@@ -0,0 +1,5 @@
+void call(app_env){
+    stage('deploy') {
+	println "deploy: deploy_to ${app_env}"
+    }
+}
--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -3,3 +3,13 @@ libraries{
   sonarqube
   notify
 }
+
+application_environments {
+    dev
+    prod
+}
+
+keywords {
+    main = ~/^[Mm]ain(line|)$/
+    develop = ~/^[Dd]evelop(ment|er|)$/
+}
