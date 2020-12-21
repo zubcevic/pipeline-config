e37d95b2df30aa967e90e28f63caa6403ee53b4f
Zubcevic.com <rene@zubcevic.com>
2020-12-21 15:01:01 +0100

    some more details

--- maven/steps/build.groovy
+++ maven/steps/build.groovy
@@ -6,10 +6,12 @@ void call(){
         //withEnv(["JAVA_HOME=${tool 'JDK8'}","PATH=${tool 'JDK8'}/bin:${tool 'MAVEN35'}/bin:$PATH"]) {
         //sh 'mvn clean install'
         //}
+        node {
         if (!isUnix()) {
             println("not a unix os")
         } else {
             println("a unix based os")
+        }
         }
 
     }
