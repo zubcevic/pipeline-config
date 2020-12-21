d9472a137d7132536ebf6c9dfb15cbcc87bb2489
Zubcevic.com <rene@zubcevic.com>
2020-12-21 14:51:41 +0100

    some more details

--- maven/steps/build.groovy
+++ maven/steps/build.groovy
@@ -2,7 +2,7 @@ void call(){
     stage('build - maven') {
 	    println "maven: build()"
         buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '5', daysToKeepStr: '30', numToKeepStr: '5')
-        checkout scm
+//checkout scm // ONLY for multi branch or pipeline from SCM
         //withEnv(["JAVA_HOME=${tool 'JDK8'}","PATH=${tool 'JDK8'}/bin:${tool 'MAVEN35'}/bin:$PATH"]) {
         //sh 'mvn clean install'
         //}
