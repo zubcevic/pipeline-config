aafedfbe63108abff0661d1bb0499d360f0d3e5e
Zubcevic.com <rene@zubcevic.com>
2020-12-21 15:57:22 +0100

    some more details

--- maven/steps/build.groovy
+++ maven/steps/build.groovy
@@ -2,13 +2,14 @@ void call(){
     stage('build - maven') {
 	    println "maven: build()"
         buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '5', daysToKeepStr: '30', numToKeepStr: '5')
-//checkout scm // ONLY for multi branch or pipeline from SCM
         
         node {
         if (!isUnix()) {
             println("not a unix os")
         } else {
             println("a unix based os")
+            checkout scm // ONLY for multi branch or pipeline from SCM
+
             withEnv(["JAVA_HOME=${tool 'JDK11'}","PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH"]) {
                 sh 'mvn clean compile'
             }
