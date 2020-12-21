e4c730d65eae8365d8f2a100d2564f89f68647d1
Zubcevic.com <rene@zubcevic.com>
2020-12-21 14:49:46 +0100

    some more details

--- maven/steps/build.groovy
+++ maven/steps/build.groovy
@@ -1,6 +1,7 @@
 void call(){
     stage('build - maven') {
 	    println "maven: build()"
+        buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '5', daysToKeepStr: '30', numToKeepStr: '5')
         checkout scm
         //withEnv(["JAVA_HOME=${tool 'JDK8'}","PATH=${tool 'JDK8'}/bin:${tool 'MAVEN35'}/bin:$PATH"]) {
         //sh 'mvn clean install'
--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -1,5 +1,6 @@
 void call(){
    stage('static code analysis') {    
-	println "sonarqube: static_code_analysis()"
+      String sonarKey = config.containsKey("sast_project_key")?config.sast_project_key:""
+      println("sonarqube: static_code_analysis() for ${sonarKey}")
    }
 }
