79a6c8bfd8572e2c089272c86a0d20a6307a8565
Zubcevic.com <rene@zubcevic.com>
2020-12-21 14:57:43 +0100

    some more details

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -1,6 +1,8 @@
 void call(){
    stage('static code analysis') {    
-      String sonarKey = config.containsKey("sast_project_key")?config.sast_project_key:""
-      println("sonarqube: static_code_analysis() for ${sonarKey}")
+      node {
+         String sonarKey = config.containsKey("sast_project_key")?config.sast_project_key:""
+         println("sonarqube: static_code_analysis() for ${sonarKey}")
+      }
    }
 }
