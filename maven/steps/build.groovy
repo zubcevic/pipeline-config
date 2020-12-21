dc3802c35a9df4020755d4e1eb38d472c342766d
Zubcevic.com <rene@zubcevic.com>
2020-12-21 15:30:27 +0100

    some more details

--- maven/steps/build.groovy
+++ maven/steps/build.groovy
@@ -3,14 +3,15 @@ void call(){
 	    println "maven: build()"
         buildDiscarder logRotator(artifactDaysToKeepStr: '30', artifactNumToKeepStr: '5', daysToKeepStr: '30', numToKeepStr: '5')
 //checkout scm // ONLY for multi branch or pipeline from SCM
-        //withEnv(["JAVA_HOME=${tool 'JDK8'}","PATH=${tool 'JDK8'}/bin:${tool 'MAVEN35'}/bin:$PATH"]) {
-        //sh 'mvn clean install'
-        //}
+        
         node {
         if (!isUnix()) {
             println("not a unix os")
         } else {
             println("a unix based os")
+            withEnv(["JAVA_HOME=${tool 'JDK11'}","PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH"]) {
+                sh 'mvn clean compile'
+            }
         }
         }
 
--- maven/steps/unit_test.groovy
+++ maven/steps/unit_test.groovy
@@ -1,8 +1,15 @@
 void call(){
     stage('maven: unit test') {
         println "maven: unit_test()"
-        withEnv(["JAVA_HOME=${tool 'JDK11'}","PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH"]) {
-            sh 'mvn test'
+        node {
+        if (!isUnix()) {
+            println("not a unix os")
+        } else {
+            println("a unix based os")
+            withEnv(["JAVA_HOME=${tool 'JDK11'}","PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH"]) {
+                sh 'mvn test'
+            }
+        }
         }
     }
 }
