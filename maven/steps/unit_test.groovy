af7fd7e343eb62fe4490bcade9d80e01e8b8ec9b
Zubcevic.com <rene@zubcevic.com>
2020-12-21 15:28:02 +0100

    some more details

--- maven/steps/unit_test.groovy
+++ maven/steps/unit_test.groovy
@@ -1,5 +1,8 @@
 void call(){
     stage('maven: unit test') {
         println "maven: unit_test()"
+        withEnv(["JAVA_HOME=${tool 'JDK11'}","PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH"]) {
+            sh 'mvn test'
+        }
     }
 }
