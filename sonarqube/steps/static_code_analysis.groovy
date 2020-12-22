7a6321d8cbceede98871cd0edf0a2674342dd14c
Zubcevic.com <rene@zubcevic.com>
2020-12-22 20:09:48 +0100

    steps

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -6,14 +6,12 @@ void call(){
          String sonarOrganization = config.containsKey("sast_project_organization")?config.sast_project_organization:""
          String sonarURL = config.containsKey("sast_project_url")?config.sast_project_url:"https://sonarcloud.io"
          println("sonarqube: static_code_analysis() for ${sonarKey}")
-         node {
             withEnv([
                "JAVA_HOME=${tool 'JDK11'}",
                "PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH",
                "SONAR_TOKEN=${sonarToken}"]) {
                 sh "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=${sonarKey} -Dsonar.organization=${sonarOrganization} -Dsonar.host.url=${sonarURL}"
             }
-         }
       }
    }
 }
