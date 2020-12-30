1db66e94185d49609656663ac789ed4786495028
Zubcevic.com <rene@zubcevic.com>
2020-12-30 14:58:33 +0100

    withCredentials added

--- sonarqube/steps/static_code_analysis.groovy
+++ sonarqube/steps/static_code_analysis.groovy
@@ -1,17 +1,22 @@
 void call(){
    stage('static code analysis') {    
-      node {
+      node { //if you open a second node block it will be in a different workspace with an @2 
+         //this workspace assumes that the buid step was done on the same node agent. perhaps better to make it parameterized
+         //or use stash unstash. but that might hinder performance
          String sonarKey = config.containsKey("sast_project_key")?config.sast_project_key:""
-         String sonarToken = config.containsKey("sast_project_token")?config.sast_project_token:""
+         String sonarTokenID = config.containsKey("sast_project_token")?config.sast_project_token:""
          String sonarOrganization = config.containsKey("sast_project_organization")?config.sast_project_organization:""
          String sonarURL = config.containsKey("sast_project_url")?config.sast_project_url:"https://sonarcloud.io"
          println("sonarqube: static_code_analysis() for ${sonarKey}")
+
+         withCredentials([string(credentialsId: 'sonarcloud-key', variable: 'sonarToken')]) {
             withEnv([
                "JAVA_HOME=${tool 'JDK11'}",
                "PATH=${tool 'JDK11'}/bin:${tool 'MAVEN363'}/bin:$PATH",
                "SONAR_TOKEN=${sonarToken}"]) {
                 sh "mvn verify org.sonarsource.scanner.maven:sonar-maven-plugin:sonar -Dsonar.projectKey=${sonarKey} -Dsonar.organization=${sonarOrganization} -Dsonar.host.url=${sonarURL}"
             }
+         }
       }
    }
 }
